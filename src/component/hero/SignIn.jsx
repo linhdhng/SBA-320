a2d13431e17cbf0b4d0e3935cb21b91e895eb32d
linhdhng <linh.duong.hng@gmail.com>
2024-03-18 09:42:20 -0500

    .

--- src/component/hero/SignIn.jsx
+++ src/component/hero/SignIn.jsx
@@ -10,7 +10,7 @@ function SignIn() {
                 <Form.Control type="email" placeholder="Enter email" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formGroupPassword">
-                <Form.Label>Password</Form.Label>
+                <Form.Label>Enter Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
             </Form.Group>
         <button style={{ borderRadius: '15px' }}>Sign In</button>
